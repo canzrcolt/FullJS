1d0468fe9fe88a64eb92c865f8267cf9c948be57
canzrcolt <canzrcolt@gmail.com>
2021-02-20 09:18:08 -0500

    added fetch

---
+++ fetchTest/fetchStuff.js
@@ -0,0 +1,18 @@
+const dog = fetch("https://reqres.in/api/users", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    name: "User 1",
+  }),
+})
+  .then((res) => {
+    if (res.ok) {
+      console.log("successr");
+    } else {
+      console.log("not successful");
+    }
+    res.json();
+  })
+  .then((data) => console.log(data));
